c4542ae7b5ab1455bde75e1334ee27317b1055db
dominique owoua <domelisa@ymail.com>
2021-08-01 12:40:06 +0000

    changes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server '44.197.43.56', user: 'ec2-user', roles: %w{ec2-user db web}
+server '44.197.43.56', user: 'ec2-user', roles: %w{app db web}
 set :ssh_options, keys: '/Users/domel/.ssh/id_rsa'
